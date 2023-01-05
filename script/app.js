648d074653632672555229c1cf02564251423e00
Vladislav Kolyanichenko <aljkeee@gmail.com>
2023-01-05 04:35:40 +0300

    feat: smooth-scroll with parallax between sections

--- script/app.js
+++ script/app.js
@@ -1,3 +1,21 @@
 window.addEventListener('scroll', e => {
-  document.body.style.cssText = `--scrollTop: ${this.scrollY}px`;
-});
+	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
+})
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+	wrapper: '.wrapper',
+	content: '.content'
+})
+
+const campfireMiddle = document.querySelector('.layers__middle-campfire');
+const campfireFront = document.querySelector('.layers__front-campfire');
+
+window.addEventListener('scroll', function () {
+  if (this.scrollY > window.innerHeight) {
+    campfireMiddle.classList.add('layers__middle-campfire_visible');
+    campfireFront.classList.add('layers__front-campfire_visibile');
+  } else {
+    campfireMiddle.classList.remove('layers__middle-campfire_visible');
+    campfireFront.classList.remove('layers__front-campfire_visibile');
+  }
+})
